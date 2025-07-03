5fb6316d85e98b67b28713b851026b2e2153ff1a
dbqdls <kkyybb0126@naver.com>
2025-07-03 17:32:04 +0900

    화이팅

--- src/main/resources/static/article/assets/scripts/index.js
+++ src/main/resources/static/article/assets/scripts/index.js
@@ -1,8 +1,20 @@
 const $information = document.querySelector('#main > .page-container >.information-review > .two-container > .content.information');
 const $review = document.querySelector('#main > .page-container >.information-review > .two-container > .content.review');
+const $informationSelect = document.querySelector('#main > .page-container >.information-review > .information-page');
+const $reviewSelect = document.querySelector('#main > .page-container >.information-review > .review-container');
 
-$information.addEventListener('click', () =>{
-    alert('1');
+$review.addEventListener('click', () =>{
+    $review.setSelected(true);
+    $information.setSelected(false);
+    $reviewSelect.setSelected(true);
+    $informationSelect.setSelected(false);
+
+
 });
 
-
+$information.addEventListener('click', () =>{
+    $review.setSelected(false);
+    $information.setSelected(true);
+    $reviewSelect.setSelected(false);
+    $informationSelect.setSelected(true);
+});
