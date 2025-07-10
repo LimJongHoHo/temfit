15ce8dfcf5ca88644726c3b8caede71ae5b85900
dbqdls <kkyybb0126@naver.com>
2025-07-10 11:49:41 +0900

    중간확인

--- src/main/java/kr/co/limbin/temfit/controllers/ItemController.java
+++ src/main/java/kr/co/limbin/temfit/controllers/ItemController.java
@@ -1,11 +1,17 @@
 package kr.co.limbin.temfit.controllers;
 
+import kr.co.limbin.temfit.entities.ReviewEntity;
+import kr.co.limbin.temfit.entities.UserEntity;
+import kr.co.limbin.temfit.results.CommonResult;
+import kr.co.limbin.temfit.results.Result;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -17,5 +23,16 @@ public class ItemController {
     public String getPay() {
         return "item/pay";
     }
+
+//    @RequestMapping(value = "/pay", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public String postPay(@SessionAttribute(value = "signedUser", required = false) UserEntity signedUser, ReviewEntity review) {
+//        Result result = this.articleService.reviewWrite(signedUser, review);
+//        JSONObject response = new JSONObject();
+//        response.put("result", result.toStringLower());
+//        if (result == CommonResult.SUCCESS) {
+//            response.put("id", review.getId());
+//        }
+//        return response.toString();
+//    }
 
 }
